a8397330fafa13d884617aa28c14062631ea62de
saides <69208665+saides-code@users.noreply.github.com>
2023-06-22 16:32:17 +0200

    cleanup

--- apps/messagesoverlay/boot.js
+++
@@ -1,7 +0,0 @@
-//override require to filter require("message")
-global.require_real=global.require;
-global.require = (_require => file => {
-    if (file==="messages") file = "messagesoverlay";
-    return _require(file);
-})(require);
-  
